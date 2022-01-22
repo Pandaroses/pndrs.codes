76de7eab09d790e3338f000b0d9516ddb1b71338
Pandaroses <georgiytnimov@gmail.com>
2022-01-22 18:56:55 +0000

    did some shit

---
+++ src/components/App.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react"
+ 
+
+
+const App = () => {
+    const [title,setTitle] = useState("");
+    const [underscore,setUnderscore] = useState(false);
+    setInterval(() => setUnderscore(!underscore),40000)
+    let finish = "Hi, I'm Gosha";
+    
+    const addletter = setInterval(() => {
+        setTitle(finish);
+    }, Math.random()*1000)   
+    
+    
+    return (
+        <div className=" bg-black-0 h-screen w-screen flex flex-col flex-nowrap justify-self-center items-center content-center">
+            <div className="basis-1/12">
+
+            </div>
+            <div className="border-2 rounded-lg border-lavender basis-3/4 align-center  text-center h-96 w-4/12">
+                <h1 className="text-teal text-6xl pt-4 ">{title+(underscore ? "_" : "")}</h1>
+            </div>
+            <div className=" basis-2/12">
+
+            </div>
+
+
+        </div>
+        
+    )
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+    theme: {
+      colors: {
+        "black": {
+          0: "#131020",
+          1: "#1a1823",
+          2: "#1e1d2f",
+          3: "#302d41"
+        },
+        "white": "#d9e0ee",
+        "red": "#f28fad",
+        "peach": "#f8bd96",
+        "yellow": "#fae3b0",
+        "green": "#abe9b3",
+        "teal": "#b5e8e0",
+        "blue": "#96cdfb",
+        "lavender": "#c9cbff"
+      },
+    fontFamily: {
+      "body": ["JetBrains Mono", "sans-serif"],
+    },
+  },
+  plugins: [],
+}
