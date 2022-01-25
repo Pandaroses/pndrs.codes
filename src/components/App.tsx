f47a6103fe0817cd73d88826d1b73ca313ff2911
Pandaroses <georgiytnimov@gmail.com>
2022-01-25 12:19:46 +0000

    shit fuck cummy bot i hate this

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,20 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useCallback } from "react"
  
 
 
 const App = () => {
     const [title,setTitle] = useState("");
     const [underscore,setUnderscore] = useState(false);
-    setInterval(() => setUnderscore(!underscore),400)
     let finish = "Hi, I'm Gosha";
-    
-    const addletter = () => setTimeout(() => {
-        setTitle(title =>title+finish[title.length]);
-        
-        title !== finish && addletter(); 
-        
-    }, 900);
+
     
     useEffect(() => {
-        addletter();
-        
-    }); 
+        setInterval(() => setUnderscore(underscore=>!underscore),400)       
+    },[]); 
     
-   
+   useEffect(() => {
+       title.length < finish.length && setTimeout(() => setTitle(title+finish[title.length]),300);
+   },[title])
     
 
     
@@ -30,7 +24,6 @@ const App = () => {
     return (
         <div className=" bg-black-0 h-screen w-screen flex flex-col flex-nowrap justify-self-center items-center content-center">
             <div className="basis-1/12">
-
             </div>
             <div className="border-2 rounded-lg border-lavender basis-3/4 align-center  text-center h-96 w-4/12 flex flex-col items-center content-center">
                 <h1 className="text-teal text-6xl pt-4 basis-4/12 ">{title+(underscore ? "_" : "")}</h1>
